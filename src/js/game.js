f902943ff2f4982b525dd836878cdc702650ff86
Michelangelo <michelangelog@gmail.com>
2021-11-07 23:17:55 +0100

    more scenes

--- src/js/game.js
+++ src/js/game.js
@@ -5,8 +5,14 @@ window.main = null;
 
 ;((self) => 
 {
+    // DEFINE
+    const FLOOR_HEIGHT = 48;
+    const JUMP_FORCE = 800;
+    const SPEED = 480;
+    const GRAVITY = 2400;
+    
     G = {};
-    G.objs = {};
+
     G.load = function()
     {
         const k = this.get_library_ref();
@@ -19,10 +25,11 @@ window.main = null;
         const k = this.get_library_ref();
         // add something to screen
         G.objs.dino = k.add([
-            k.sprite("dino", {jumpForce:740}),
-            k.pos(80, 40),
+            k.sprite("dino"),
+            k.pos(280, 40),
             k.area(),
             k.body(),
+            k.z(10)
         ]);
     };
     
@@ -31,65 +38,142 @@ window.main = null;
         const k = G.get_library_ref();
         // add platform
         k.add([
-            k.rect(k.width(), 48),
-            k.pos(0, k.height() - 48),
+            k.rect(k.width(), FLOOR_HEIGHT),
+            k.pos(0, k.height() ),
+            k.origin("botleft"),
             k.outline(4),
             k.area(),
             k.solid(),
             k.color(127, 200, 254),
-        ]);
-    
-        // add tree
-        k.add([
-            k.rect(48, 64),
-            k.area(),
-            k.outline(4),
-            k.pos(k.width(), k.height() - 48),
-            k.origin("botleft"),
-            k.color(255, 180, 255),
-            k.move(k.LEFT, 240),
-            "tree"
-        ]);
+        ]);    
     };
     
     G.bind = function()
     {
         const k = G.get_library_ref();
         const dino = G.objs.dino;
-        // .jump() when "space" key is pressed
-        k.onKeyPress("space", () =>
+        
+        const jump = () =>
         {
             if(dino.isGrounded())
             {
-                dino.jump(650);
-            }
-        });
+                dino.jump(JUMP_FORCE);
+            }            
+        };
 
-        k.onTouchStart((id, pos) =>
-        {
-            if(dino.isGrounded())
-            {
-                dino.jump(650);
-            }
-        });
-    
+        k.onKeyPress("space", () => jump());
+        k.onTouchStart((id, pos) => jump());
+        k.onMousePress((btn, pos) => jump());
+        
         dino.onCollide("tree", () => {
             k.addKaboom(dino.pos);
+            k.burp();
             k.shake();
+            k.wait(0.5, () =>
+            {
+                k.go('game_over', this.vars.score);
+            });
         });
+
+        k.onUpdate(() =>
+        {
+            this.vars.score++;
+            this.objs.score_lbl.text = this.vars.score;
+        });
+    
+    };
+
+    G.spawn_enemy = function()
+    {
+        const k = G.get_library_ref();
+        k.add(
+        [
+            k.rect(48, k.rand(32, 96)),
+            k.area(),
+            k.outline(4),
+            k.pos(k.width(), k.height() - FLOOR_HEIGHT),
+            k.origin("botleft"),
+            k.color(255, 180, 255),
+            k.move(k.LEFT, SPEED),
+            k.z(5),
+            "tree"
+        ]);
+        k.wait(k.rand(0.5, 1.5), () =>
+        {
+            this.spawn_enemy();
+        });   
+    };
+
+    G.game_over_screen = function(score)
+    {
+        const k = G.get_library_ref();
+        k.add(
+        [
+            k.sprite('dino'),
+            k.pos(k.width() / 2, k.height() / 2 - 80),
+            k.scale(2),
+            k.origin("center"),
+        ]);
+    
+        // display score
+        k.add(
+        [
+            k.text(score),
+            k.pos(k.width() / 2, k.height() / 2 + 80),
+            k.scale(2),
+            k.origin("center"),
+        ]);
+    
+        // go back to game with space is pressed
+        k.onKeyPress("space", () => k.go('game_run'));
+        k.onClick(() => k.go('game_run'));
+    };
+
+    G.create_score = function()
+    {
+        const k = G.get_library_ref();
+        this.vars.score = 0;
+        this.objs.score_lbl = k.add(
+        [
+            k.text(this.vars.score),
+            k.pos(24, 24)
+        ]);
     };
     
     G.init = function(lib)
     {
-        // getting reference to game library
+        // init hash for game objects
+        this.objs = {};
+
+        // init hash for game vars
+        this.vars = {};
+
+        // setting reference to game library
         G.get_library_ref = () =>
         {
             return lib;
         }
         G.load();
-        G.add_base();
-        G.add_player();
-        G.bind();
+
+        // main scene
+
+        lib.scene('game_run', () =>
+        {
+            lib.gravity(GRAVITY);
+            G.add_base();
+            G.add_player();
+            G.create_score();
+            G.spawn_enemy();
+            G.bind();
+        });
+
+        // game over scene
+        lib.scene('game_over', (score) =>
+        {
+            G.game_over_screen(score);
+        });
+
+        lib.go('game_run');
     };
 
     // entry point
