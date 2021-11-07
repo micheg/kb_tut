82d8e52125106b2f80608552bc7d07c236020ddf
Michelangelo <michelangelog@gmail.com>
2021-11-07 18:52:48 +0100

    jump

--- src/js/game.js
+++ src/js/game.js
@@ -2,16 +2,32 @@ import kaboom from "kaboom";
 
 kaboom();
 
-add([
-    text("hello world"),
-    pos(120, 80),
-]);
-
 // load a sprite "bean" from an image
 loadSprite("dino", "img/sprites/dino.png")
 
 // add something to screen
-add([
+const dino = add([
     sprite("dino"),
     pos(80, 40),
-])
+    area(),
+    body(),
+]);
+
+// add platform
+add([
+    rect(width(), 48),
+    pos(0, height() - 48),
+    outline(4),
+    area(),
+    solid(),
+    color(127, 200, 255),
+]);
+
+// .jump() when "space" key is pressed
+onKeyPress("space", () =>
+{
+    if(dino.isGrounded())
+    {
+        dino.jump()
+    }
+});
